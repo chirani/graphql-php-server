4d4de320aa7e71fc9a01952c8a14aeb99c7eb226
b.chirani <chirani124@gmail.com>
2026-03-15 02:08:13 +0100

    setup php two

---
+++ src/Entity/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'products')]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int|null $id = null;
+
+    #[ORM\Column(type: 'string')]
+    private string $name;
+}
