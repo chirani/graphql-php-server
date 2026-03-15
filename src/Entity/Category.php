f419b161c96690c7bfeb6e8b8bf5e830a39d8c77
b.chirani <chirani124@gmail.com>
2026-03-15 03:54:01 +0100

    updated entites

---
+++ src/Entity/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'categories')]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "string")]
+    private string $id;
+
+    #[ORM\Column(type: "string")]
+    private string $name;
+
+    #[ORM\OneToMany(mappedBy: 'products', targetEntity: Product::class)]
+    private Collection $products;
+
+    public function __construct(string $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->products = new ArrayCollection();
+    }
+}
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -11,8 +11,27 @@ class Product
     #[ORM\Id]
     #[ORM\Column(type: 'integer')]
     #[ORM\GeneratedValue]
-    private int|null $id = null;
+    private ?int $id = null;
 
     #[ORM\Column(type: 'string')]
     private string $name;
+
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'products')]
+    #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id', nullable: false)]
+    private ?Category $category = null;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(Category $category): void
+    {
+        $this->category = $category;
+    }
 }
