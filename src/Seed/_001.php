40f3dcf82f735819bef3552c2527eb4bf68000bc
b.chirani <chirani124@gmail.com>
2026-03-17 17:53:46 +0100

    Added _001 categroy seed file

---
+++ src/Seed/_001.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Entity\Category;
+
+require_once "bootstrap.php";
+
+
+$json = file_get_contents(__DIR__ . "/data.json");
+$json_data = json_decode($json, true);
+$data = $json_data["data"];
+$categories = $data["categories"];
+
+foreach ($categories as $category) {
+
+    $new_category = new Category($category["name"], $category["name"]);
+
+    $entityManager->persist($new_category);
+}
+
+$entityManager->flush();
+
+echo "Categories seeded successfully\n";
